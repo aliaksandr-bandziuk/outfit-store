5e9e26b4ea1bb369a2181f0f1cdde0f64f2dab68
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2022-12-18 14:03:23 +0100

    feat: create pages files

---
+++ src/pages/FullOutfit/FullOutfit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FullOutfit = () => {
+  return (
+    <section className="outfit">
+      <div className="container">
+
+      </div>
+    </section>
+  )
+}
+
+export default FullOutfit;
