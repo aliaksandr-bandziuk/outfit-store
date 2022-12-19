8b4bf4159a77b093bc68050e816b613bbc1972b7
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2022-12-19 19:17:39 +0100

    feat: create redux store

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Currency.module.scss';
 
 import arrow from '../../assets/img/currency-arrow.svg';
 
+export const currencyList = [
+  { name: 'zł PLN', currencyProperty: 'pln' },
+  { name: '€ EUR', currencyProperty: 'eur' },
+  { name: '$ USD', currencyProperty: 'usd' }
+]
+
 const Currency = () => {
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={styles.currency}>
-      <p className={styles.name}>$ USD</p>
-      <svg
-        width="8"
-        height="4"
-        viewBox="0 0 8 4"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
-        <path
-        d="M1 0.5L4 3.5L7 0.5"
-        stroke="black"
-        strokeLinecap="round"
-        strokeLinejoin="round"/>
-      </svg>
+    <div className={styles.sort}>
+      <div onClick={() => setOpen(!open)} className={styles.currency}>
+        <p className={styles.name}>$ USD</p>
+        <svg
+          width="8"
+          height="4"
+          viewBox="0 0 8 4"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+          d="M1 0.5L4 3.5L7 0.5"
+          stroke="black"
+          strokeLinecap="round"
+          strokeLinejoin="round"/>
+        </svg>
+      </div>
+      {
+        open ? (
+          <div className={styles.currencyPopup}>
+            <ul className={styles.list}>
+              {
+                currencyList.map((obj, index) => (
+                  <li
+                    key={index}
+                    className={styles.sort.currencyProperty === obj.currencyProperty ? 'active' : ''}
+                  >
+                  {obj.name}
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        ) : ''
+      }
+
     </div>
   )
 }
---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+}
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    }
+  },
+})
+
+console.log(filterSlice.actions);
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = filterSlice.actions
+
+export default filterSlice.reducer
